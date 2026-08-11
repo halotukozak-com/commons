a9e507ecb4bdbfc7a8604ed1a98b37990dd2b1ac
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-08-11 15:55:47 +0200

    init commit

---
+++ project.scala
@@ -0,0 +1,24 @@
+//> using scala 3.8.4
+
+//> using test.dep org.scalameta::munit::1.3.5
+//> using test.dep org.scala-lang::scala3-compiler:3.8.4
+
+//> using options -deprecation -feature -new-syntax -unchecked
+//> using options -language:noAutoTupling
+//> using options -Vprofile -Xprint-inline
+//> using options -Ycheck:macros -Ydebug-flags -Ydebug-missing-refs
+//> using options -Ycheck:all
+//> using options -Yexplain-lowlevel -Yexplicit-nulls
+//> using options -Yshow-suppressed-errors -Yshow-var-bounds
+//> using options -Wsafe-init -Werror -Wunused:all
+////> using options -Yprofile-enabled" -Yprofile-trace:debug/compile-trace.json"
+
+//> using publish.organization io.github.halotukozak
+//> using publish.name commons
+//> using publish.computeVersion git:tag
+//> using publish.description "halotukozak's commons"
+//> using publish.url https://github.com/halotukozak/commons
+//> using publish.license MIT
+//> using publish.vcs github:halotukozak/commons
+//> using publish.repository central
+//> using publish.developer "halotukozak|Bartłomiej Kozak|https://github.com/halotukozak"
